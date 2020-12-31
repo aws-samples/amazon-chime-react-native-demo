7cf2944bd77068041260c62e0b00011bdacab06f
Sang Lin <55719628+linsang21@users.noreply.github.com>
2020-12-31 12:49:37 -0800

    Upgrade jest version (#44)

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,13 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import { Login } from '../src/containers/Login';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', () => {
+  const tree = renderer.create(<Login />).toJSON();
+  // jest snapshot testing: https://jestjs.io/docs/en/snapshot-testing
+  expect(tree).toMatchSnapshot()
 });
