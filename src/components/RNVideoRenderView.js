2f1aff5d8a72787bcf9c1923401e0b6412b78344
Nick Hwang <57926812+hokyungh@users.noreply.github.com>
2023-06-15 16:21:53 -0400

    Fix null node (#189)

--- src/components/RNVideoRenderView.js
+++ src/components/RNVideoRenderView.js
@@ -15,12 +15,15 @@ export class RNVideoRenderView extends React.Component {
     // Because "componentDidMount" will be called "immediately after the initial rendering occurs"
     // This is *before* RCTUIManager add this view to register (so that viewForReactTag() can return a view)
     // So we need to dispatch bindVideoView after this function complete
-    setTimeout(() => {
+    // We need to clearTimeout https://github.com/aws/amazon-chime-sdk-android/issues/563 to avoid crashes when setTimeout occurs
+    // After view is dismounted.
+    this.timeoutId = setTimeout(() => {
       NativeFunction.bindVideoView(findNodeHandle(this), this.props.tileId);
     });
   }
 
   componentWillUnmount() {
+    clearTimeout(this.timeoutId);
     NativeFunction.unbindVideoView(this.props.tileId);
   }
 
